943d5481c5e0efcc879933218fa505de15806048
Samira Jahan <samirashammi67@gmail.com>
2023-09-27 00:19:38 +0600

    task 1 commit

---
+++ task_1.php
@@ -0,0 +1,12 @@
+<?php
+function modifyText($text) {
+    
+    $text = strtolower($text);  
+    $text = str_replace("brown", "red", $text);
+    echo $text;
+}
+
+$text = "The quick brown fox jumps over the lazy dog.";
+
+modifyText($text);
+?>
